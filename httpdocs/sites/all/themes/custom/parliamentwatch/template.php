594c0d49149d17a2c769b37aae7864c4357de1e4
Stefan Freudenberg <stefan@agaric.com>
2018-09-10 09:31:39 +0200

    Remove unused variable

--- httpdocs/sites/all/themes/custom/parliamentwatch/template.php
+++ httpdocs/sites/all/themes/custom/parliamentwatch/template.php
@@ -209,7 +209,6 @@ function parliamentwatch_preprocess_block(&$variables) {
       $parliament = $variables['elements']['#parliament'];
       $variables['parliament'] = $parliament->name;
       $variables['theme_hook_suggestions'][] = 'block__pw_globals__title__dialogues';
-      $variables['user_display_name'] = _pw_get_fullname($variables['elements']['#account']);
       $variables['user_party'] = pw_profiles_party($account)->name;
       $variables['user_url'] = url(entity_uri('user', $account)['path']);
     }
