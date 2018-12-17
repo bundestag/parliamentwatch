3ee89f83a35d90e5f305cd94f6031bb3ffa5fc9a
Timon Weber <hi@timonweber.com>
2018-12-17 14:54:59 +0100

    AW-6020 Show explanation for all list elections

--- httpdocs/sites/all/themes/custom/parliamentwatch/templates/user-profile--full.tpl.php
+++ httpdocs/sites/all/themes/custom/parliamentwatch/templates/user-profile--full.tpl.php
@@ -186,7 +186,7 @@
         <?php endif; ?>
         <?php if (!empty($user_profile['field_user_list'])): ?>
           <dt class="dl__dt"><?php print $user_profile['field_user_list']['#title']; ?></dt>
-          <dd class="dl__dd"><?php print $user_profile['field_user_list'][0]['#markup']; ?><?php if ($user_profile['field_user_list_position']): ?>, Platz <?php print $user_profile['field_user_list_position'][0]['#markup']; ?><?php endif; ?><?php if (!isset($field_user_election_result)): ?> (<?php print t('drafted via list'); ?>)<?php endif; ?></dd>
+          <dd class="dl__dd"><?php print $user_profile['field_user_list'][0]['#markup']; ?><?php if ($user_profile['field_user_list_position']): ?>, Platz <?php print $user_profile['field_user_list_position'][0]['#markup']; ?><?php endif; ?><?php if ($user_profile['field_user_list_won']['#items'][0]['value'] == 1): ?> (<?php print t('drafted via list'); ?>)<?php endif; ?></dd>
         <?php endif; ?>
         <?php if (!empty($user_profile['field_user_parliament'])): ?>
           <dt class="dl__dt"><?php print $user_profile['field_user_parliament']['#title']; ?></dt>
