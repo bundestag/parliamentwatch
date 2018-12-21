872f0844ddff068d03695dacc4ed43b1cf9a9d44
Tobias Krause <krause@abgeordnetenwatch.de>
2018-12-21 14:44:38 +0100

    AW-6026 DataAnswer class

--- httpdocs/sites/all/modules/custom/pw_datatransfers/src/Modtool/DrupalEntity/DataAnswer.php
+++ httpdocs/sites/all/modules/custom/pw_datatransfers/src/Modtool/DrupalEntity/DataAnswer.php
@@ -4,15 +4,33 @@
 namespace Drupal\pw_datatransfers\Modtool\DrupalEntity;
 
 
+use stdClass;
+
 class DataAnswer extends DataEntityBase {
 
 
   public function createDrupalEntity() {
-    // TODO: Implement createDrupalEntity() method.
+    $modtoolMessage = $this->modtoolMessage;
+    $answer = new stdClass();
+    $answer->language = LANGUAGE_NONE;
+    $answer->type = 'dialogue';
+    $this->isNew = TRUE;
+    $answer->title = 'Frage von ' . $modtoolMessage->getSenderName();
+    node_object_prepare($answer);
+
+    $answer->uid = 0;
+    return $answer;
   }
 
   public function loadDrupalEntity() {
-    // TODO: Implement loadDrupalEntity() method.
+    $modtoolAnswer = $this->modtoolMessage;
+    $question = NULL;
+
+    if ($message_id = $modtoolAnswer->getMessageId()) {
+      $question = load_answer_by_message_id($message_id);
+    }
+
+    return $question;
   }
 
   public function setDrupalEntityValuesFromJson($entity) {
