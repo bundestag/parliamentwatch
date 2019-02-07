e7ce527dd6767c38426ce49eb9aebe3056050eb6
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2019-02-07 23:37:44 +0100

    AWT-243 Set correct theme-wrapper

--- httpdocs/sites/all/themes/custom/parliamentwatch/template.php
+++ httpdocs/sites/all/themes/custom/parliamentwatch/template.php
@@ -120,18 +120,18 @@ function parliamentwatch_page_alter(&$page) {
 
     $has_container = !empty(array_filter($page['content']['system_main']['#theme_wrappers'], $filter));
     $has_filters = isset($page['content']['system_main']['filters']);
-    $is_blog_or_petition_or_dialogue_page_or_poll_page_or_committee_page = menu_get_object('node') ? in_array(menu_get_object('node')->type, ['blogpost', 'dialogue', 'pw_petition', 'poll', 'committee']) : FALSE;
+    $is_blog_or_petition_or_dialogue_page_or_poll_page_or_committee_page_or_press_release = menu_get_object('node') ? in_array(menu_get_object('node')->type, ['blogpost', 'dialogue', 'pw_petition', 'poll', 'committee', 'press_release']) : FALSE;
     $is_profile_page = in_array(menu_get_item()['page_callback'], ['user_view_page', 'user_revision_show']);
     $is_comment_reply_page = menu_get_item()['page_callback'] == 'comment_reply';
     $is_topic_page = menu_get_item()['page_callback'] == 'pw_globals_taxonomy_term_page';
     $press_page_view = views_get_page_view();
-    if ($press_page_view->name === 'press_articles') {
+    if ($press_page_view->name === 'press_articles' || $press_page_view->name === 'press_release') {
       $is_press_page = true;
     } else {
       $is_press_page = false;
     };
 
-    if (!$has_container && !$has_filters && !$is_blog_or_petition_or_dialogue_page_or_poll_page_or_committee_page && !$is_profile_page && !$is_comment_reply_page && !$is_topic_page && !$is_press_page) {
+    if (!$has_container && !$has_filters && !$is_blog_or_petition_or_dialogue_page_or_poll_page_or_committee_page_or_press_release && !$is_profile_page && !$is_comment_reply_page && !$is_topic_page && !$is_press_page) {
       $page['content']['system_main']['#prefix'] = '<div class="container">';
       $page['content']['system_main']['#suffix'] = '</div>';
     }
@@ -308,7 +308,7 @@ function parliamentwatch_preprocess_node(&$variables) {
     $variables['content']['field_teaser_image_copyright'] = field_view_field('file', file_load($variables['field_teaser_image'][0]['fid']), 'field_image_copyright', 'default');
   }
 
-  if ($variables['type'] == 'blogpost' && $variables['view_mode'] == 'full') {
+  if ($variables['type'] == 'blogpost' && $variables['view_mode'] == 'full' || $variables['type'] == 'press_release' && $variables['view_mode'] == 'full') {
     $variables['username'] = _pw_get_fullname(user_load($node->uid));
     $variables['date'] = format_date($node->created, 'short');
     $variables['submitted'] = t('Submitted by !username on !datetime', array('!username' => $variables['username'], '!datetime' => $variables['date']));
