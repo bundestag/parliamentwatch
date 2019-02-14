b9b31fdbde426fb4240e1080bd63aaf01767b2ea
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2019-02-14 10:46:45 +0100

    AWT-243 Use l-function to build link

--- httpdocs/sites/all/themes/custom/parliamentwatch/templates/node--press-release--tile.tpl.php
+++ httpdocs/sites/all/themes/custom/parliamentwatch/templates/node--press-release--tile.tpl.php
@@ -12,6 +12,8 @@
     <p><?php print render($content['field_press_release_body']); ?></p>
   </div>
   <ul class="tile__links tile__links--2">
-    <li class="tile__links__item"><a class="tile__links__item__link" href="<?php print $node_url ?>"><?php print t('read more'); ?></a></li>
+    <li class="tile__links__item">
+      <?php print l(t('read more'), $node_url, array('attributes' => array('class' => array('tile__links__item__link')))); ?>
+    </li>
   </ul>
 </article>
