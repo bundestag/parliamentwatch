5656acd6472b8a3df7f3526454a77e4dae2c219a
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2019-02-14 19:48:25 +0100

    AWT-244 Define list via theme_item_list

--- httpdocs/sites/all/themes/custom/parliamentwatch/templates/block--pw-press--press-links.tpl.php
+++ httpdocs/sites/all/themes/custom/parliamentwatch/templates/block--pw-press--press-links.tpl.php
@@ -13,63 +13,7 @@
 <div class="press-links">
   <div class="container-regular">
     <?php print render($title_suffix) ?>
-    <ul class="press-links__list">
-      <li class="press-links__list__item press-links__list__item--1">
-        <?php print l(
-          '<i class="press-links__list__item__icon icon icon-folders"></i>' . t('Press Review'),
-          'ueber-uns/presse/pressespiegel',
-          array(
-            'attributes' => array('class' => array('press-links__list__item__link')),
-            'html' => TRUE
-          )); ?>
-      </li>
-      <li class="press-links__list__item press-links__list__item--2">
-        <?php print l(
-          '<i class="press-links__list__item__icon icon icon-folders"></i>' . t('Press releases'),
-          'ueber-uns/presse/pressemitteilungen',
-          array(
-            'attributes' => array('class' => array('press-links__list__item__link')),
-            'html' => TRUE
-          )); ?>
-      </li>
-      <li class="press-links__list__item press-links__list__item--3">
-        <?php print l(
-          '<i class="press-links__list__item__icon icon icon-thesis"></i>' . t('Press newsletter'),
-          'node/972122',
-          array(
-            'attributes' => array('class' => array('press-links__list__item__link')),
-            'html' => TRUE
-          )); ?>
-      </li>
-      <li class="press-links__list__item press-links__list__item--4">
-        <?php print l(
-          '<i class="press-links__list__item__icon icon icon-download"></i>' . t('Press package'),
-          'node/39911',
-          array(
-            'attributes' => array('class' => array('press-links__list__item__link')),
-            'html' => TRUE
-          )); ?>
-      </li>
-      <li class="press-links__list__item press-links__list__item--5">
-        <?php print l(
-          '<i class="press-links__list__item__icon icon icon-group"></i>' . t('Contributors'),
-          'node/7751',
-          array(
-            'attributes' => array('class' => array('press-links__list__item__link')),
-            'html' => TRUE
-          )); ?>
-      </li>
-      <li class="press-links__list__item press-links__list__item--6">
-        <?php print l(
-          '<i class="press-links__list__item__icon icon icon-mail"></i>' . t('Contact'),
-          'node/6578',
-          array(
-            'attributes' => array('class' => array('press-links__list__item__link')),
-            'html' => TRUE
-          )); ?>
-      </li>
-
-    </ul>
+    <?php print render($content) ?>
   </div>
 </div>
 
