0c190b31dac88dc16a43557c72623586999342dd
Timon Weber <hi@timonweber.com>
2019-03-04 09:30:06 +0100

    AWT-354 Display election result as decimal number

--- httpdocs/sites/all/themes/custom/parliamentwatch/templates/user-profile--full.tpl.php
+++ httpdocs/sites/all/themes/custom/parliamentwatch/templates/user-profile--full.tpl.php
@@ -178,7 +178,7 @@
               <p>
                 <small>
                   <?php print $user_profile['field_user_election_result'][0]['#title']; ?>:
-                  <?php print $user_profile['field_user_election_result'][0]['#markup']; ?>
+                  <?php print number_format(floatval($user_profile['field_user_election_result'][0]['#markup']), 1, ',', '.'); ?> %
                 </small>
               </p>
             <?php endif; ?>
