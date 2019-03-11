351f32d385b121c3e83808d08f3a27a313e4c92e
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2019-03-11 12:01:32 +0100

    AWT-337 Optimize frontend styling of paragraph-items

--- httpdocs/sites/all/themes/custom/parliamentwatch/templates/paragraphs-item--landingpage_contributor_group.tpl.php
+++ httpdocs/sites/all/themes/custom/parliamentwatch/templates/paragraphs-item--landingpage_contributor_group.tpl.php
@@ -6,6 +6,8 @@
  */
 ?>
 <div class="tile-wrapper tile-wrapper--contributor"<?php print $attributes; ?>>
-  <h2><?php print render($content['field_landingpage_headline']); ?></h2>
+  <?php if ($content['field_landingpage_headline']): ?>
+    <h2><?php print render($content['field_landingpage_headline']); ?></h2>
+  <?php endif; ?>
   <?php print render($content['field_landingpage_contributor']); ?>
 </div>
