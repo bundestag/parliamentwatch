e9c7f4451e012a7db5dd54fd953ba90af5e20b82
Christopher Neugebauer <christopher.neugebauer.87@gmail.com>
2019-03-27 22:44:33 +0100

    AWT-110 Add custom form-template

---
+++ httpdocs/sites/all/themes/custom/parliamentwatch/templates/node--65995--full.tpl.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Add container-small to sticker-webform
+ */
+?>
+<div class="container-small">
+  <div id="node-<?php print $node->nid; ?>" class="webform-container clearfix <?php print $classes; ?>"<?php print $attributes; ?>>
+    <?php print render($title_prefix); ?>
+    <h1><?php print $title; ?></h1>
+    <?php print render($title_suffix); ?>
+
+    <?php if ($display_submitted): ?>
+      <div class="submitted">
+        <?php print $submitted; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+    print render($content);
+    ?>
+
+    <?php print render($content['links']); ?>
+
+    <?php print render($content['comments']); ?>
+
+  </div>
+</div>
