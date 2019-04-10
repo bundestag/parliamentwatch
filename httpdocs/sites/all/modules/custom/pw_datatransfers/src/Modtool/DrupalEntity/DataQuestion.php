bc0bd52b48a8b64d473e20e9cb9df44a180b7c49
Tobias Krause <krause@abgeordnetenwatch.de>
2019-04-10 13:32:20 +0200

    AWT-529 avoid double decoding

--- httpdocs/sites/all/modules/custom/pw_datatransfers/src/Modtool/DrupalEntity/DataQuestion.php
+++ httpdocs/sites/all/modules/custom/pw_datatransfers/src/Modtool/DrupalEntity/DataQuestion.php
@@ -93,8 +93,8 @@ class DataQuestion extends DataEntityBase {
     $node->body = [
       LANGUAGE_NONE => [
         0 => [
-          'value' => json_decode($modtoolMessage->getText()),
-          'summary' => json_decode($modtoolMessage->getSummary()),
+          'value' => $modtoolMessage->getText(),
+          'summary' => $modtoolMessage->getSummary(),
           'format' => 'filtered_html',
         ],
       ],
