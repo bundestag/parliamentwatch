1f0c6c7b9e9550098161d40de16aa8a1a60cfc48
Timon Weber <hi@timonweber.com>
2019-04-15 14:28:46 +0200

    AWT-442 Add contact to press release template

--- httpdocs/sites/all/themes/custom/parliamentwatch/templates/node--press-release--full.tpl.php
+++ httpdocs/sites/all/themes/custom/parliamentwatch/templates/node--press-release--full.tpl.php
@@ -11,4 +11,23 @@
       </header>
       <?php print render($content['field_press_release_body']); ?>
     </div>
+  </div>
 </article>
+
+<div class="container-small">
+  <aside class="press-contact">
+    <div class="press-contact__image">
+      <img src="/<?php print drupal_get_path('theme', 'parliamentwatch'); ?>/images/lea_briand.jpg" alt="Léa Briand">
+    </div>
+
+    <div class="press-contact__content">
+      <h1 class="press-contact__title h3"><?php print t('Contact person for press inquiries'); ?></h1>
+      <p class="press-contact__subtitle">Léa Briand, <?php print t('Press spokeswoman'); ?></p>
+
+      <div class="press-contact__details">
+        <div class="press-contact__detail"><?php print t('Phone'); ?>: 040 / 3176910 23</div>
+        <div class="press-contact__detail"><?php print t('Email'); ?>: <a href="mailto:briand@abgeordnetenwatch.de">briand@abgeordnetenwatch.de</a></div>
+      </div>
+    </div>
+  </aside>
+</div>
